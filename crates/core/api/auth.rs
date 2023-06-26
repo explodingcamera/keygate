12f4d496ac4c38b1e684a4a49ecb145a61d3b1c0
Henry Gressmann <mail@henrygressmann.de>
2023-06-26 22:13:01 +0200

    feat: more auth functions
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- crates/core/api/auth.rs
+++ crates/core/api/auth.rs
@@ -3,7 +3,6 @@ use std::{net::IpAddr, sync::Arc};
 use keygate_utils::random::secure_random_id;
 use prisma::{identity, PrismaClient};
 use proto::api::auth::*;
-use tonic::{Request, Response, Status};
 
 use super::APIError;
 use crate::KeygateInternal;
@@ -22,31 +21,29 @@ impl Auth {
         &self.keygate.prisma
     }
 
-    async fn login(&self, req: InitLoginRequest) -> Result<LoginResponse, APIError> {
+    async fn login(&self, username_or_email: String, ip_address: String) -> Result<LoginResponse, APIError> {
         let client = self.client();
 
-        let ip = req.ip_address;
-        if ip.is_empty() || ip.parse::<IpAddr>().is_err() {
+        if ip_address.is_empty() || ip_address.parse::<IpAddr>().is_err() {
             return Err(APIError::invalid_argument("Invalid IP address"));
         }
 
         let login_process_id = secure_random_id();
         let _login_process_id = login_process_id.clone(); // we need to clone this for the closure
 
-        let is_email = req.username_or_email.contains('@');
+        let is_email = username_or_email.contains('@');
         let current_step = match is_email {
             true => LoginStep::Email,
             false => LoginStep::Username,
         };
 
         let next_steps = client
-            ._transaction()
-            .run::<APIError, _, _, _>(|client| async move {
+            .tx::<APIError, _, _, _>(|client| async move {
                 let current_identity = client
                     .identity()
                     .find_unique(match is_email {
-                        true => identity::primary_email::equals(req.username_or_email),
-                        false => identity::username::equals(req.username_or_email),
+                        true => identity::primary_email::equals(username_or_email),
+                        false => identity::username::equals(username_or_email),
                     })
                     .exec()
                     .await?
@@ -64,6 +61,8 @@ impl Auth {
                     .exec()
                     .await?;
 
+                // next step is always password for now
+                // TODO: device login
                 Ok(vec![LoginStep::Password as i32])
             })
             .await?;
@@ -76,19 +75,69 @@ impl Auth {
         })
     }
 
-    async fn login_step(&self, request: Request<LoginStepRequest>) -> Result<Response<LoginResponse>, Status> {
+    async fn login_step(&self, request: LoginStepRequest) -> Result<LoginResponse, APIError> {
+        let next_steps: Option<Vec<LoginStep>> = self
+            .client()
+            .tx::<APIError, _, _, _>(|client| async move {
+                let current_process = client
+                    .login_process()
+                    .find_unique(prisma::login_process::UniqueWhereParam::IdEquals(request.process_id))
+                    .exec()
+                    .await?
+                    .ok_or(APIError::not_found("Login process not found"))?;
+
+                let current_step = LoginStep::from_str_name(&current_process.current_step)
+                    .ok_or(APIError::invalid_argument("Invalid step type"))?;
+                let step = LoginStep::from_i32(request.step_type).ok_or(APIError::invalid_argument("Invalid step type"))?;
+
+                match (current_step, step) {
+                    (LoginStep::Email, LoginStep::Password) | (LoginStep::Username, LoginStep::Password) => {
+                        // TODO: Implement password login
+                    }
+                    _ => return Err(APIError::invalid_argument("Invalid step type")),
+                };
+
+                // TODO: Check if more steps are required (e.g. 2FA)
+                Ok(None)
+            })
+            .await?;
+
         unimplemented!()
     }
 
-    async fn login_status(&self, request: Request<LoginStatusRequest>) -> Result<Response<LoginStatusResponse>, Status> {
-        unimplemented!()
+    async fn login_status(&self, request: LoginStatusRequest) -> Result<LoginStatusResponse, APIError> {
+        let process = self
+            .client()
+            .login_process()
+            .find_unique(prisma::login_process::UniqueWhereParam::IdEquals(request.process_id))
+            .exec()
+            .await?
+            .ok_or(APIError::not_found("Login process not found"))?;
+
+        Ok(LoginStatusResponse {
+            current_step: LoginStep::from_str_name(&process.current_step)
+                .ok_or(APIError::invalid_argument("Invalid step type"))?
+                .into(),
+            expires_at: process.expires_at.timestamp(),
+        })
     }
 
-    async fn account_exists(&self, request: Request<AccountExistsRequest>) -> Result<Response<AccountExistsResponse>, Status> {
-        unimplemented!()
+    async fn account_exists(&self, request: AccountExistsRequest) -> Result<AccountExistsResponse, APIError> {
+        let exists = self
+            .client()
+            .identity()
+            .find_unique(match request.request.ok_or(APIError::invalid_argument("Missing request"))? {
+                account_exists_request::Request::Email(email) => identity::primary_email::equals(email),
+                account_exists_request::Request::Username(username) => identity::username::equals(username),
+            })
+            .exec()
+            .await?
+            .is_some();
+
+        Ok(AccountExistsResponse { exists })
     }
 
-    async fn signup(&self, request: Request<SignupRequest>) -> Result<Response<SignupResponse>, Status> {
+    async fn signup(&self, request: SignupRequest) -> Result<SignupResponse, APIError> {
         unimplemented!()
     }
 }
--- crates/core/prisma/lib.rs
+++ crates/core/prisma/lib.rs
@@ -1,7 +1,21 @@
 mod prisma;
+use std::future::Future;
+
+use client::QueryError;
 pub use prisma::*;
 pub use prisma_client_rust as client;
 
 pub async fn connect(url: &str) -> Result<PrismaClient, client::NewClientError> {
     PrismaClient::_builder().with_url(url.into()).build().await
+}
+
+impl PrismaClient {
+    pub async fn tx<TErr, TRet, TFut, TFn>(&self, tx: TFn) -> Result<TRet, TErr>
+    where
+        TFut: Future<Output = Result<TRet, TErr>>,
+        TFn: FnOnce(PrismaClient) -> TFut,
+        TErr: From<QueryError>,
+    {
+        self._transaction().run(tx).await
+    }
 }
