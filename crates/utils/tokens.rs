e88d6396380984f139497128a64cb8041f0b4b19
Henry Gressmann <mail@henrygressmann.de>
2023-06-28 20:39:05 +0200

    feat(utils): initial paesto support
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- crates/utils/tokens.rs
+++ crates/utils/tokens.rs
@@ -1,135 +1,137 @@
-use keygate_jwt::{
-    prelude::{
-        Audiences, Claims, Duration, Ed25519KeyPair, EdDSAKeyPairLike, EdDSAPublicKeyLike, JWTClaims, NoCustomClaims, UnixTimeStamp,
-        VerificationOptions,
-    },
-    JWTError,
-};
+use chrono::{Duration, Utc};
+use rusty_paseto::prelude::*;
+use thiserror::Error;
 
-pub fn generate_session_id() -> String {
-    random::secure_random_id()
+use crate::random::secure_random_id;
+
+#[derive(Error, Debug)]
+pub enum TokenError {
+    #[error(transparent)]
+    PasetoError(#[from] PasetoError),
+
+    #[error(transparent)]
+    PasetoClaimError(#[from] PasetoClaimError),
+
+    #[error("Failed to generate token")]
+    FailedToGenerateToken,
+
+    #[error("Invalid token")]
+    InvalidToken,
+    #[error("Expired token")]
+    ExpiredToken,
+
+    #[error("Other error: {0}")]
+    Other(String),
 }
 
-pub fn generate_access_token_id() -> String {
-    random::secure_random_id()
+pub struct AccessToken(pub String);
+pub struct RefreshToken(pub String);
+
+pub struct Keypair {
+    pub id: String,
+    private_key: Key<64>,
+    public_key: Key<32>,
 }
 
-pub fn generate_refresh_token_id() -> String {
-    random::secure_random_id()
-}
-use super::random;
-pub struct RefreshToken(String);
+pub struct GenericKey(String);
+impl<const KEYSIZE: usize> TryFrom<Key<KEYSIZE>> for GenericKey {
+    type Error = TokenError;
 
-pub struct KeygateClaims {
-    pub issued_at: UnixTimeStamp,
-    pub expires_at: UnixTimeStamp,
-    pub issuer: String,
-    pub subject: String,
-    pub audiences: Audiences,
-    pub jwt_id: String,
+    fn try_from(value: Key<KEYSIZE>) -> Result<Self, Self::Error> {
+        hex::encode(value.as_ref())
+            .parse()
+            .map_err(|_| TokenError::Other("Failed to parse private key".to_string()))
+            .map(Self)
+    }
 }
 
-impl TryInto<KeygateClaims> for JWTClaims<NoCustomClaims> {
-    type Error = JWTError;
+pub type PrivateKey = GenericKey;
+pub type PublicKey = GenericKey;
 
-    fn try_into(self) -> Result<KeygateClaims, Self::Error> {
-        if let (Some(issued_at), Some(expires_at), Some(issuer), Some(subject), Some(audiences), Some(jwt_id)) = (
-            self.issued_at,
-            self.expires_at,
-            self.issuer,
-            self.subject,
-            self.audiences,
-            self.jwt_id,
-        ) {
-            Ok(KeygateClaims {
-                issued_at,
-                expires_at,
-                issuer,
-                subject,
-                audiences,
-                jwt_id,
-            })
-        } else {
-            Err(JWTError::NotJWT)
+impl Keypair {
+    pub fn serialize(&self) -> (String, PrivateKey, PublicKey) {
+        (
+            self.id.clone(),
+            self.private_key
+                .clone()
+                .try_into()
+                .expect("internal private key value should never be invalid"),
+            self.public_key
+                .clone()
+                .try_into()
+                .expect("internal public key value should never be invalid"),
+        )
+    }
+
+    pub fn generate() -> Self {
+        let private_key = Key::<64>::try_new_random()
+            .map_err(|_| TokenError::Other("Failed to generate private key".to_string()))
+            .unwrap();
+
+        let public_key = Key::<32>::try_new_random()
+            .map_err(|_| TokenError::Other("Failed to generate public key".to_string()))
+            .unwrap();
+
+        let key_id = secure_random_id();
+
+        Self {
+            id: key_id.clone(),
+            private_key,
+            public_key,
         }
     }
-}
 
-pub struct AccessToken(String);
+    pub fn import(id: String, private_key: &str, public_key: &str) -> Result<Self, TokenError> {
+        let private_key = Key::<64>::try_from(private_key).map_err(|_| TokenError::Other("Invalid private key".to_string()))?;
+        let public_key = Key::<32>::try_from(public_key).map_err(|_| TokenError::Other("Invalid public key".to_string()))?;
 
-impl AccessToken {
-    pub fn new(token: String) -> Self {
-        Self(token)
+        Ok(Self {
+            id,
+            private_key,
+            public_key,
+        })
     }
 
-    pub fn verify(&self, key_pair: &Ed25519KeyPair) -> Result<KeygateClaims, JWTError> {
-        let options = VerificationOptions { ..Default::default() };
-        let claims: JWTClaims<NoCustomClaims> = key_pair.public_key().verify_token(&self.0, Some(options))?;
-        claims.try_into()
+    fn paseto_private_key(&self) -> PasetoAsymmetricPrivateKey<V4, Public> {
+        PasetoAsymmetricPrivateKey::<V4, Public>::from(self.private_key.as_slice())
     }
 
-    pub fn generate(identity_id: &str, audience: &str, expires_in_seconds: i64, key_pair: Ed25519KeyPair) -> Result<Self, JWTError> {
-        let claims = Claims::create(Duration::from_secs(expires_in_seconds.unsigned_abs()))
-            .with_issuer("keygate")
-            .with_audience(audience)
-            .with_subject(identity_id)
-            .with_jwt_id(generate_access_token_id());
-
-        let token = key_pair.sign(claims)?;
-        Ok(token.into())
+    fn paseto_public_key(&self) -> PasetoAsymmetricPublicKey<V4, Public> {
+        PasetoAsymmetricPublicKey::<V4, Public>::from(&self.public_key)
     }
-}
 
-impl From<AccessToken> for String {
-    fn from(token: AccessToken) -> Self {
-        token.0
+    fn generate_refresh_token(
+        &self,
+        identity_id: &str,
+        session_id: &str,
+        audience: &str,
+        subject: &str,
+        issuer: &str,
+    ) -> Result<RefreshToken, TokenError> {
+        let in_5_minutes = duration_to_rfc3339(Duration::minutes(5));
+
+        let token_id = secure_random_id();
+
+        let refresh_token = PasetoBuilder::<V4, Public>::default()
+            .set_claim(ExpirationClaim::try_from(in_5_minutes)?)
+            .set_claim(AudienceClaim::from(audience))
+            .set_claim(SubjectClaim::from(subject))
+            .set_claim(IssuerClaim::from(issuer))
+            .set_claim(TokenIdentifierClaim::from(token_id.as_str()))
+            .build(&self.paseto_private_key());
+
+        unimplemented!();
     }
-}
 
-impl RefreshToken {
-    pub fn new(refresh_token_id: &str) -> Self {
-        Self(refresh_token_id.to_string())
+    fn generate_access_token() -> Result<AccessToken, TokenError> {
+        unimplemented!();
     }
-}
 
-impl From<RefreshToken> for String {
-    fn from(token: RefreshToken) -> Self {
-        token.0
-    }
-}
-
-impl From<String> for RefreshToken {
-    fn from(token: String) -> Self {
-        RefreshToken(token)
-    }
-}
-
-impl From<String> for AccessToken {
-    fn from(token: String) -> Self {
-        AccessToken(token)
+    fn rotate_refresh_token() -> Result<(RefreshToken, AccessToken), TokenError> {
+        unimplemented!();
     }
 }
 
-// test this
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use keygate_jwt::prelude::Ed25519KeyPair;
-
-    #[test]
-    fn test_refresh_token() {
-        let token = RefreshToken::new(&generate_refresh_token_id());
-        assert_eq!(token.0.len(), 21);
-    }
-
-    #[test]
-    fn test_signed_access_token() {
-        let key_pair = Ed25519KeyPair::generate();
-        let token = AccessToken::generate("identity_id", "audience", 3600, key_pair.clone()).unwrap();
-        assert_eq!(token.0.len(), 299);
-        let claims = token.verify(&key_pair).unwrap();
-        assert_eq!(claims.issuer, "keygate");
-        assert_eq!(claims.subject, "identity_id");
-        assert_eq!(claims.audiences, Audiences::AsString("audience".to_string()));
-    }
+pub fn duration_to_rfc3339(duration: Duration) -> String {
+    (Utc::now() + duration).to_rfc3339()
 }
