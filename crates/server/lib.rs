9494b83c9939e86d2e674d8c79f137c9b08d64c8
Henry Gressmann <mail@henrygressmann.de>
2023-08-25 14:40:37 +0200

    chore: poem setup
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- crates/server/lib.rs
+++ crates/server/lib.rs
@@ -4,9 +4,12 @@
 #![allow(clippy::enum_variant_names)]
 
 mod errors;
-mod services;
+mod private;
+mod public;
 
 use keygate_core::{config::Environment, Keygate, KeygateConfig};
+use poem::{listener::TcpListener, Route};
+use poem_openapi::{LicenseObject, OpenApiService};
 
 pub async fn run(config: KeygateConfig) -> eyre::Result<()> {
     if config.environment == Environment::Development {
@@ -15,17 +18,42 @@ pub async fn run(config: KeygateConfig) -> eyre::Result<()> {
 
     let keygate = Keygate::new(config).await?;
 
-    // let addr = "[::1]:50051".parse().unwrap();
+    let private_api = OpenApiService::new(private::PrivateApi, "Keygate Private API", "v0")
+        .description("The private API for Keygate, used for backend communication.")
+        .license(
+            LicenseObject::new("Apache-2.0")
+                .identifier("Apache-2.0")
+                .url("https://www.apache.org/licenses/LICENSE-2.0.html"),
+        )
+        .server("/api/private/v0");
 
-    // let gprc_server = Server::builder()
-    //     .add_service(keygate.identity.service())
-    //     .add_service(keygate.identity.service());
+    let public_api = OpenApiService::new(public::PublicApi, "Keygate Public API", "v0")
+        .description("The public API for Keygate, used for frontend communication.")
+        .license(
+            LicenseObject::new("Apache-2.0")
+                .identifier("Apache-2.0")
+                .url("https://www.apache.org/licenses/LICENSE-2.0.html"),
+        )
+        .server("/api/public/v0");
 
-    // tokio::select! {
-    //     _ = gprc_server.serve(addr) => {
-    //         println!("gRPC server stopped");
-    //     }
-    // };
+    let private_api_swagger = private_api.swagger_ui();
+    let public_api_swagger = public_api.swagger_ui();
+
+    let private_app = Route::new()
+        .nest("/api/private/v0", private_api)
+        .nest("/openapi", private_api_swagger);
+
+    let public_app = Route::new()
+        .nest("/api/public/v0", public_api)
+        .nest("/openapi", public_api_swagger);
+
+    let private_server = poem::Server::new(TcpListener::bind("127.0.0.1:3000")).run(private_app);
+    let public_server = poem::Server::new(TcpListener::bind("127.0.0.1:3001")).run(public_app);
+
+    tokio::select! {
+        res = private_server => res?,
+        res = public_server => res?,
+    }
 
     Ok(())
 }
---
+++ crates/server/public/mod.rs
@@ -0,0 +1,15 @@
+use poem::web::Query;
+use poem_openapi::{payload::PlainText, OpenApi};
+
+pub struct PublicApi;
+
+#[OpenApi]
+impl PublicApi {
+    #[oai(path = "/hello", method = "get")]
+    async fn index(&self, name: Query<Option<String>>) -> PlainText<String> {
+        match name.0 {
+            Some(name) => PlainText(format!("hello, {}!", name)),
+            None => PlainText("hello!".to_string()),
+        }
+    }
+}
